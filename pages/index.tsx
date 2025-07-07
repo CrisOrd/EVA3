e24d301d77fe4ebc01b47a3e8c2de5dedaa2234e
Cristobal Ordenes <cristobal.ordenes01@gmail.com>
2025-07-06 22:14:31 -0400

    Creacion de Formulario
    Paso 2 de la EVA3
    
    Se ha creado el formulario, incluyendo todas las etiquetas requeridas según la pauta de evaluación. Además, se eliminó todo el contenido del archivo globals.css debido a que interfería con la implementación de las etiquetas en el archivo index.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,56 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+'use client'
 export default function Home() {
+ 
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <form>
+      <h1>Registro de Eventos</h1>
+      <br />
+      <label>Nombre del Evento </label><br />
+      <input
+        name="nombreEvento" 
+        type="text" 
+        placeholder="Nombre del Evento"
+        /><br />
+      <span></span>
+
+      <label>Costo del Evento </label><br />
+      <input 
+        name ="costeEvento"
+        type="number" 
+        min = "0"
+        max = "100000000"
+        placeholder="Costo $"
+        
+        /><br />
+      <span></span>
+
+      <label>Tipo de Evento</label><br />
+      <select name="tipoEvento">Tipo Del Evento
+        <option value="">Seleccione el Tipo de Evento</option>
+        <option value="Conferencia">Conferencia</option>
+        <option value="Taller">Taller</option>
+        <option value="Seminario">Seminario</option>
+        <option value="Matrimonio">Matrimonio</option>
+        <option value="Cumpleaños">Cumpleaños</option>
+        <option value="Baby Shower">Baby Shower</option>
+      </select><br />
+      <span></span>
+      <label>Descripcion del Evento</label>
+      <br />
+      <textarea name="descripcionEvento"></textarea>
+      <span></span>
+      <br />
+      <label>Fecha del Evento</label><br />
+      <input
+        name="fechaEvento"
+        type="date"
+        min="2025-7-01"
+        max="2030-12-31"
+        placeholder="Fecha del Evento"
+      /><br />
+      <span></span>
+      <button>Registrar</button>
+
+    </form>
+    );
 }
