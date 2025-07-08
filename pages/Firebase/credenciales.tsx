5b782adb57939425b1ff6be3cf68216d5812e128
Cristobal Ordenes <cristobal.ordenes01@gmail.com>
2025-07-08 11:49:03 -0400

    Primera version de implementacion de firebase
    Se instalo firebase y se esta empezando a implementar en el codigo para introducir los datos del evento en una base de datos

---
+++ pages/Firebase/credenciales.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA75zZbVupMJr3Zp5K7TX3l9UcgJ6lu-xw",
+    authDomain: "eva4-9437f.firebaseapp.com",
+    projectId: "eva4-9437f",
+    storageBucket: "eva4-9437f.firebasestorage.app",
+    messagingSenderId: "1084546424329",
+    appId: "1:1084546424329:web:ba951ebd031dc1be9562d8",
+    measurementId: "G-TSCNNN7W34"
+  };
